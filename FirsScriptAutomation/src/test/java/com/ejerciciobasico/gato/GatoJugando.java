bb7b6aecb66a2e0cf5b088730c4b51d4e12c3733
cristiancossio3268 <cristian.cossio3268@hotmail.com>
2021-08-07 20:04:34 -0400

    Ejercicio N1 - java basico OK

--- FirsScriptAutomation/src/test/java/com/ejerciciobasico/gato/GatoJugando.java
+++ FirsScriptAutomation/src/test/java/com/ejerciciobasico/gato/GatoJugando.java
@@ -5,35 +5,23 @@ public class GatoJugando {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		isCatPlaying (false, 37);
+		isCatPlaying (false, 36);
 	}
 	
-	public static string isCatPlaying(boolean verano, int temperatura) {
-
-		if (verano = true){
-			if (temperatura >= 25){				
-			    if (temperatura <= 45) {
-			       String gatito = "Verdadero";
-			       return gatito;
-				   System.out.println("Verdadero "+ temperatura);
-			   }else {
-				   System.out.println("Falso "+ temperatura);
-			   }	
-			}else {
-				System.out.println("Falso "+ temperatura);
-			}
-		}else if(verano = false){
-			if (temperatura >= 25){				
-			    if (temperatura <= 35) {
-				   System.out.println("Verdadero "+ temperatura);
-			   }else {
-				   System.out.println("Falso "+ temperatura);
-			   }	
-			}else {
-				System.out.println("Falso "+ temperatura);
-			}
-		}
+	public static void isCatPlaying(boolean verano, int temperatura) {
 		
-	}
-
-}
+		if (verano == true){
+			if (temperatura >= 25 && temperatura <= 45){	
+				   System.out.println("Verdadero "+ temperatura);			   
+		   }else {
+				   System.out.println("Falso "+ temperatura);
+			   }
+		}else if(verano == false){
+			if (temperatura >= 25 && temperatura <= 35){
+				   System.out.println("Verdadero "+ temperatura);
+			}else {
+				   System.out.println("Falso "+ temperatura);
+			      }	
+		    }	
+	   }
+   }
